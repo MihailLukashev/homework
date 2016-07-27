e36f791f9076033e20d71bfffc96e5316ebb6c2a
Mihail <mihail.lukashev@duniceedge.com>
2016-07-27 16:47:41 +0300

    Glava 11

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
@@ -58,11 +59,9 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1.json
   def destroy
     @line_item = LineItem.find(params[:id])
-    if @line_item.quantity > 1
-      @line_item.update_attributes(:quantity => @line_item.quantity - 1)
-    else
+
       @line_item.destroy
-    end
+
     respond_to do |format|
       format.html { redirect_to(cart_url(session[:cart_id])) }
       format.json { head :no_content }
